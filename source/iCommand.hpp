98177942cfa394b2475520ac8872c8bd577f630a
Victor Leskin <victorleskin16@gmail.com>
2025-06-09 23:19:41 +0300

    Initial version

---
+++ source/iCommand.hpp
@@ -0,0 +1,10 @@
+///************************* OUTS HOMEWORK ****************************************
+#ifndef ICOMMAND_HPP
+#define ICOMMAND_HPP
+
+class iCommand
+{
+public:
+};
+
+#endif //#ifndef ICOMMAND_HPP
